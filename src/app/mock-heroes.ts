23fc96f9083517d694188b8b673f81bd4de6299c
Softfun <xyf18362929560@163.com>
2019-03-15 00:24:17 +0800

    first init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './heroes/hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: '盖伦'},
+  {id: 12, name: '贾克斯'},
+  {id: 13, name: '狗头'},
+  {id: 14, name: '拉克丝'},
+  {id: 15, name: '阿卡丽'},
+  {id: 16, name: '猴子'},
+  {id: 17, name: '劫'},
+  {id: 18, name: '薇恩'},
+  {id: 19, name: '卡萨丁'},
+  {id: 20, name: '石头人'}
+];
